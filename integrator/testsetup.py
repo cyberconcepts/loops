360cdef11b7d7d1d1084bcc5589f424820da0211
helmutm <helmutm@fd906abe-77d9-0310-91a1-e0d9ade77398>
2007-04-10 14:58:46 +0000

    new loops subpackage 'integrator' for importing and integrating operating system files and other external objects
    git-svn-id: svn://svn.cy55.de/Zope3/src/loops/trunk@1683 fd906abe-77d9-0310-91a1-e0d9ade77398

--- integrator/testsetup.py
+++ integrator/testsetup.py
@@ -5,29 +5,13 @@ $Id$
 """
 
 from zope import component
-from zope.app.catalog.catalog import Catalog
-from zope.app.catalog.interfaces import ICatalog
-from zope.app.catalog.field import FieldIndex
-from zope.app.catalog.text import TextIndex
 
-from cybertools.relation.tests import IntIdsStub
-from cybertools.relation.registry import RelationRegistry
-from cybertools.relation.interfaces import IRelationRegistry
-from cybertools.relation.registry import IndexableRelationAdapter
-from cybertools.typology.interfaces import IType
-
-from loops import Loops
 from loops import util
-from loops.interfaces import IIndexAttributes
 from loops.concept import Concept
-from loops.concept import IndexAttributes as ConceptIndexAttributes
 from loops.resource import Resource
-from loops.resource import IndexAttributes as ResourceIndexAttributes
 from loops.integrator.interfaces import IExternalCollection
 from loops.knowledge.setup import SetupManager as KnowledgeSetupManager
-from loops.setup import SetupManager, addObject
 from loops.tests.setup import TestSite as BaseTestSite
-from loops.type import ConceptType, ResourceType, TypeConcept
 
 
 class TestSite(BaseTestSite):
