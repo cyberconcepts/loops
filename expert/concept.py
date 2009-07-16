412440dcfbb10849b8ed5207a123b95382f972c5
helmutm <helmutm@fd906abe-77d9-0310-91a1-e0d9ade77398>
2009-07-16 10:19:56 +0000

    make FullQuery also usable for full-text queries without children
    git-svn-id: svn://svn.cy55.de/Zope3/src/loops/trunk@3459 fd906abe-77d9-0310-91a1-e0d9ade77398

--- expert/concept.py
+++ expert/concept.py
@@ -114,12 +114,16 @@ class BaseQuery(object):
 class FullQuery(BaseQuery):
 
     def query(self, text=None, type=None, useTitle=True, useFull=False,
-                    conceptTitle=None, conceptUid=None, conceptType=None, **kw):
+                    conceptTitle=None, conceptUid=None, conceptType=None,
+                    withChildren=True, **kw):
         result = set()
         scores = {}
         intids = component.getUtility(IIntIds)
-        rc = self.queryConceptsWithChildren(title=conceptTitle, uid=conceptUid,
-                                            type=conceptType)
+        if withChildren:
+            rc = self.queryConceptsWithChildren(title=conceptTitle, uid=conceptUid,
+                                                type=conceptType)
+        else:
+            rc = self.queryConcepts(title=conceptTitle, type=type)
         if not rc and not text and '*' in type: # there should be some sort of selection...
             return ScoredSet(result, scores)
         if text or type != 'loops:*':  # TODO: this may be highly inefficient!
