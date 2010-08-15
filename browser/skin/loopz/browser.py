ede833e425b976d03334d60cb97a57142952a86b
helmutm <helmutm@fd906abe-77d9-0310-91a1-e0d9ade77398>
2010-08-15 09:23:16 +0000

    create subdirectories for indiviual skins; add Lobo skin (grid-base layout using Blueprint)
    git-svn-id: svn://svn.cy55.de/Zope3/src/loops/trunk@3953 fd906abe-77d9-0310-91a1-e0d9ade77398

---
+++ browser/skin/loopz/browser.py
@@ -0,0 +1,31 @@
+#
+#  Copyright (c) 2006 Helmut Merz helmutm@cy55.de
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+"""
+Dummy view class for providing the body template.
+
+$Id$
+"""
+
+from cybertools.browser.view import UnboundTemplateFile
+
+
+class View(object):
+
+    bodyTemplate = UnboundTemplateFile('body.pt')
+
