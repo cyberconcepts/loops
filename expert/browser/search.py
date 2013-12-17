8495eea8ada2e60789c3c1464422b51c9e8db0f2
Helmut Merz <helmutm@cy55.de>
2013-12-17 09:43:25 +0100

    avoid retrieving a list of all concepts for filtering select widget

--- expert/browser/search.py
+++ expert/browser/search.py
@@ -141,7 +141,7 @@ class Search(ConceptView):
             title = None
         types = request.get('searchType')
         data = []
-        if title or types:
+        if title or (types and types != u'loops:concept:*'):
             if title is not None:
                 title = title.replace('(', ' ').replace(')', ' ').replace(' -', ' ')
                 #title = title.split(' ', 1)[0]
