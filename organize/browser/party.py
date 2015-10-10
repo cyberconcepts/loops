11f859b71e75534c1d1df69ebb59840cc7e5b22c
Helmut Merz <helmutm@cy55.de>
2015-10-10 11:20:02 +0200

    provide BCC to sender, controlled by class variable

--- organize/browser/party.py
+++ organize/browser/party.py
@@ -189,6 +189,8 @@ class SendEmailForm(NodeView):
 
 class SendEmail(FormController):
 
+    bccToSender = False
+
     def checkPermissions(self):
         return (not self.isAnonymous and 
                 super(SendEmail, self).checkPermissions())
@@ -205,7 +207,10 @@ class SendEmail(FormController):
         msg = MIMEText(message.encode('utf-8'), 'plain', 'utf-8')
         msg['Subject'] = subject.encode('utf-8')
         msg['From'] = sender
-        msg['To'] = ', '.join(r.strip() for r in recipients if r.strip())
+        recipients = [r.strip() for r in recipients if r.strip()]
+        msg['To'] = ', '.join(recipients)
+        if self.bccToSender:
+            recipients.append(sender)
         mailhost = component.getUtility(IMailDelivery, 'Mail')
         mailhost.send(sender, recipients, msg.as_string())
         return True
