ffa5c8701e521af1f95ae4342296aad16a82c309
Helmut Merz <helmutm@cy55.de>
2024-09-29 08:47:05 +0200

    work in progress: loops / bluebream instances (app installations)

---
+++ inst/bluebream/config.py
@@ -0,0 +1,19 @@
+# loops/inst/bluebream/config.py
+
+from dotenv import load_dotenv
+from os import getenv
+from scopes.server.app import zope_app_factory
+
+load_dotenv()
+
+server_port = getenv('SERVER_PORT', '8099')
+
+app_factory = zope_app_factory
+
+# storage settings
+from scopes.storage.db.postgres import StorageFactory
+dbengine = 'postgresql+psycopg'
+dbname = getenv('DBNAME', 'demo')
+dbuser = getenv('DBUSER', 'demo')
+dbpassword = getenv('DBPASSWORD', 'secret')
+dbschema = getenv('DBSCHEMA', 'demo')
---
+++ inst/bluebream/main.py
@@ -0,0 +1,20 @@
+# loops/inst/bluebream/main.py
+
+from wsgiref.simple_server import make_server
+
+def run(app, config):
+    port = int(config.server_port)
+    with make_server('', port, app) as httpd:
+        print(f'Serving on port {port}.')
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print('Shutting down.')
+
+
+if __name__ == '__main__':
+    import config
+    #run(config.app, config)
+    app = config.app_factory(config)
+    run(app, config)
+    # see zope.app.wsgi.getWSGIApplication
